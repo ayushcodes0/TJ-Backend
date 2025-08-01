8f24d26963ad69ef520efb723652422668a6de16
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-08-01 09:26:18 +0530

    Profile image shown on login done successfully

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -104,7 +104,8 @@ exports.login = async (req, res) => {
         id: user._id,
         username: user.username,
         email: user.email,
-        subscription: user.subscription
+        subscription: user.subscription,
+        avatar: user.avatar
       },
       success: true,
       error: false
